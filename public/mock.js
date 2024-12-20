b514e269e5d96cb4607c69e8edc3e3a61f857fd3
ipqw <iga538741@gmail.com>
2024-12-20 08:07:44 +0400

    init

---
+++ public/mock.js
@@ -0,0 +1,18 @@
+export const locations = [{
+    region: 'lksjdglk',
+    address: 'werqwerwer',
+    date: Date.now(),
+    freeZones: 124
+}, 
+{
+    region: 'lksjdglk',
+    address: 'werqwerwer',
+    date: Date.now(),
+    freeZones: 124
+}, 
+{
+    region: 'lksjdglk',
+    address: 'werqwerwer',
+    date: Date.now(),
+    freeZones: 124
+}]
